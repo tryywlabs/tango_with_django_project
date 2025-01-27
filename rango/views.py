08fbb42f3fcd60e2946a6b63221e41ce509a9e8b
Yongwoo Hur <2867744h@student.gla.ac.uk>
2025-01-27 17:03:50 +0000

    additional rango changes

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello World, I'm Rango!")
+  return HttpResponse("Hello World, I'm Rango! </br> <a href='/rango/about/'>About</a>")
+
+def about(request):
+  return HttpResponse("Rango says here is the about page. </br> <a href='/rango/'>Home</a>")
